e8dfa6518e8a5cc34500ab80434af641b735858f
Dominik <domstach00@gmail.com>
2024-02-12 14:00:01 +0100

    Added basic insert trip dialog

---
+++ webapp/src/app/dialog/trip-table-add-new-dialog/trip-table-add-new-dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import {
+  TripPlanTableAddNewDialogComponent
+} from "../trip-plan-table-add-new-dialog/trip-plan-table-add-new-dialog.component";
+import { Trip } from "../../_model/trip";
+
+@Component({
+  selector: 'app-trip-table-add-new-dialog',
+  templateUrl: './trip-table-add-new-dialog.component.html',
+  styleUrls: ['./trip-table-add-new-dialog.component.scss']
+})
+export class TripTableAddNewDialogComponent {
+  constructor(
+    readonly dialogRef: MatDialogRef<TripPlanTableAddNewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Trip
+  ) {
+  }
+
+  onCloseClick(): void {
+    this.dialogRef.close();
+  }
+}
