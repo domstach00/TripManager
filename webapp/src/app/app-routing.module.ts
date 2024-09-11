07228f9f5e67abfa86800daf1d375b143965d9eb
Dominik <domstach00@gmail.com>
2024-09-11 19:13:45 +0200

    Fix routing

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -9,13 +9,11 @@ import { TripPlanComponent } from "./trips/trip-plan/trip-plan.component";
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
-	{ path: '**', redirectTo: '/login'  },
 	{ path: 'login', component: LoginFormComponent },
 	{ path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
 	{ path: 'settings', component: HomeComponent, canActivate: [AuthGuard] },
-	{ path: 'trips', component: TripsComponent, canActivate: [AuthGuard], children: [
-			{ path: 'trips/:tripId', component: TripPlanComponent, canActivate: [AuthGuard] },
-		] },
+	{ path: 'trips', component: TripsComponent, canActivate: [AuthGuard] },
+	{ path: 'trips/:tripId', component: TripPlanComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
