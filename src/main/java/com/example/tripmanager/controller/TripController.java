b2959f8cb02e09fa288b1b014059192155f0e996
Dominik <domstach00@gmail.com>
2024-10-01 16:39:07 +0200

    Add page params to TripController

--- src/main/java/com/example/tripmanager/controller/TripController.java
+++ src/main/java/com/example/tripmanager/controller/TripController.java
@@ -1,14 +1,13 @@
 package com.example.tripmanager.controller;
 
+import com.example.tripmanager.controller.support.PageParams;
 import com.example.tripmanager.model.Trip;
 import com.example.tripmanager.model.TripDto;
 import com.example.tripmanager.service.AccountService;
 import com.example.tripmanager.service.TripService;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -30,10 +29,8 @@ public class TripController extends AbstractController {
     @GetMapping
     public Page<TripDto> getTrips(
             Principal principal,
-            @RequestParam(name = "pageSize", required = false, defaultValue = "5") int pageSize,
-            @RequestParam(name = "pageNumber", required = false, defaultValue = "0") int pageNumber
+            @ParameterObject PageParams pageParams
     ) {
-        Pageable pageable = buildPageable(pageSize, pageNumber, "ASC");
-        return Trip.toDto(this.tripService.getTripsForAccount(pageable, accountService.getCurrentAccount()));
+        return Trip.toDto(this.tripService.getTripsForAccount(pageParams.asPageable(), accountService.getCurrentAccount()));
     }
 }
