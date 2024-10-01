db10a18a2cc35ac27d8c64ce97f245d9f67c47f3
Dominik <domstach00@gmail.com>
2024-10-01 16:38:47 +0200

    Create PageParams to data in pages

--- src/main/java/com/example/tripmanager/controller/AbstractController.java
+++ src/main/java/com/example/tripmanager/controller/AbstractController.java
@@ -1,12 +1,5 @@
 package com.example.tripmanager.controller;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 public abstract class AbstractController {
-
-    protected Pageable buildPageable(int pageSize, int pageNumber, String sort) {
-        return PageRequest.of(pageNumber, pageSize);
-    }
 }
