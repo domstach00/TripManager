660879f4c7daf34dd1d73e80cfdf48136e2544e6
Dominik <domstach00@gmail.com>
2024-10-03 13:30:37 +0200

    Set default language as en

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AuthService } from "./_services/auth.service";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
 	selector: 'app-root',
@@ -7,7 +7,10 @@ import { AuthService } from "./_services/auth.service";
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	constructor(readonly authService: AuthService) {
+	constructor(
+		readonly translate: TranslateService,
+	) {
+		translate.setDefaultLang('en');
 	}
 
 
