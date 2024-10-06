56a07bb056de69da726f3e903050cd3a99469d19
Dominik <domstach00@gmail.com>
2024-10-06 22:02:51 +0200

    Create AuditableMapper

---
+++ src/main/java/com/example/tripmanager/mapper/AuditableMapper.java
@@ -0,0 +1,31 @@
+package com.example.tripmanager.mapper;
+
+import com.example.tripmanager.exception.AccountNotFoundException;
+import com.example.tripmanager.model.AbstractAuditable;
+import com.example.tripmanager.model.AbstractAuditableDto;
+import com.example.tripmanager.model.account.Account;
+import com.example.tripmanager.service.AccountService;
+
+import java.util.Objects;
+
+public class AuditableMapper {
+    public static <T extends AbstractAuditable, DTO extends AbstractAuditableDto> DTO toDto(T auditable, DTO auditableDto, AccountService accountService) {
+        if (auditable == null || auditableDto == null) {
+            return null;
+        }
+        auditableDto.setId(auditable.getId());
+        auditableDto.setCreatedTime(auditable.getCreatedTime());
+        auditableDto.setLastModifiedTime(auditable.getLastModifiedTime());
+
+        if (accountService != null) {
+            final Account createdBy = accountService.getAccountById(auditable.getCreatedBy()).orElseThrow(AccountNotFoundException::new);
+            auditableDto.setCreatedBy(AccountMapper.toDto(createdBy));
+
+            final Account lastModifiedBy = Objects.equals(createdBy.getId(), auditable.getLastModifiedBy())
+                    ? createdBy
+                    : accountService.getAccountById(auditable.getLastModifiedBy()).orElseThrow(AccountNotFoundException::new);
+            auditableDto.setLastModifiedBy(AccountMapper.toDto(lastModifiedBy));
+        }
+        return auditableDto;
+    }
+}
