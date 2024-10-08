8653630cddb80b5cf7fe83abbb979d8f22d0b0c8
Dominik <domstach00@gmail.com>
2024-10-08 15:34:04 +0200

    Improve trips table

--- webapp/src/app/trips/trips-table.component.ts
+++ webapp/src/app/trips/trips-table.component.ts
@@ -3,6 +3,7 @@ import { SearchResultComponent } from "../shared/directives/search-result/search
 import { Trip } from "../_model/trip";
 import { TranslateService } from "@ngx-translate/core";
 import { RouterService } from "../_services/router.service";
+import { DateUtilService } from "../_services/date-util.service";
 
 @Component({
   selector: 'app-trips-table',
@@ -15,12 +16,17 @@ export class TripsTableComponent extends SearchResultComponent<Trip> {
 	constructor(
 		protected override readonly translate: TranslateService,
 		protected readonly routerService: RouterService,
+		protected readonly dateUtilService: DateUtilService,
 	) {
 		super(translate);
 	}
 
 	navigateToTrip(tripId: string) {
 		this.routerService.navToTrip(tripId);
+	}
+
+	formatDate(date: Date): string {
+		return this.dateUtilService.formatDate(date);
 	}
 
 }
