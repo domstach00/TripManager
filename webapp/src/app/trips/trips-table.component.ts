01f79ce8bbe6e802fb3403785f254e081d494bb4
Dominik <domstach00@gmail.com>
2024-10-08 14:39:44 +0200

    Added tooltip and ability to move to given trip page

--- webapp/src/app/trips/trips-table.component.ts
+++ webapp/src/app/trips/trips-table.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { SearchResultComponent } from "../shared/directives/search-result/search-result.component";
 import { Trip } from "../_model/trip";
+import { TranslateService } from "@ngx-translate/core";
+import { RouterService } from "../_services/router.service";
 
 @Component({
   selector: 'app-trips-table',
@@ -10,5 +12,15 @@ import { Trip } from "../_model/trip";
 export class TripsTableComponent extends SearchResultComponent<Trip> {
 	displayedColumns: string[] = ['tripName', 'dayLength', 'summaryCost', 'lastUpdate']
 
+	constructor(
+		protected override readonly translate: TranslateService,
+		protected readonly routerService: RouterService,
+	) {
+		super(translate);
+	}
+
+	navigateToTrip(tripId: string) {
+		this.routerService.navToTrip(tripId);
+	}
 
 }
