be2295d417eec8a15aa053ffb030bfc2fd6006b2
Dominik <domstach00@gmail.com>
2025-02-09 10:15:23 +0100

    Fix SubCategory collection name

--- src/main/java/com/example/tripmanager/budget/model/category/SubCategory.java
+++ src/main/java/com/example/tripmanager/budget/model/category/SubCategory.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = SubCategory.COLLECTION_NAME)
 public class SubCategory extends AbstractAuditable {
-    public static final String COLLECTION_NAME = "Categories";
+    public static final String COLLECTION_NAME = "SubCategories";
     public static final String FIELD_NAME_NAME = "name";
     private String name;
 
